0b2da95488d6e6eae9d6e98f4335b2956bd499a1
Jordan Love <jlove934@uw.edu>
2015-04-20 16:52:25 -0700

    Starting main.c file

---
+++ Project_2/main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ *  	Created on: Apr 20, 2015
+ *      Author: GameTime
+ */
+
+#include<stdio.h>
+
+int main(int argc, char** argv) {
+  printf("Hello World\n");
+  return 0;
+}
