d4cdd6fd9f2c3a426c51730734aac4ac57cb1ac2
Jordan Love <jlove934@uw.edu>
2015-04-20 18:21:42 -0700

    I believe I have the matrix multiplication working. Do check to make sure my math is right lol Now we just have to implement pthreads

--- Project_2/main.c
+++ Project_2/main.c
@@ -6,9 +6,83 @@
  */
 
 #include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int** createMatrix(int size);
+void printMatrix(int **matrix, int size);
+int** multiplyMatrix (int **firstMatrix, int **secondMatrix, int size);
 
 int main(int argc, char** argv) {
-  printf("Hello World\n");
+  //Create the instance for the Rand()
+  srand(time(NULL));
+  int matrixSize = 16;
+
+  //Set both matrices with random numbers 0-99
+  int** firstMatrix = createMatrix(matrixSize);
+  int** secondMatrix = createMatrix(matrixSize);
+
+  //Print the first two matrices
+  printf("First Matrix!\n");
+  printMatrix(firstMatrix, matrixSize);
+  printf("Second Matrix!\n");
+  printMatrix(secondMatrix, matrixSize);
+
+  //Product of the first two matrices
+  int** finalMatrix = multiplyMatrix(firstMatrix, secondMatrix, matrixSize);
+
+  //Print the Product of the first two matrices
+  printf("Product Matrix!\n");
+  printMatrix(finalMatrix, matrixSize);
 
   return 0;
+}
+
+int** createMatrix(int size) {
+	int** matrix = malloc(size * sizeof(int *));
+
+	int i, j;
+
+	for (i = 0; i < size; i++) {
+		matrix[i] = malloc(size * sizeof(int));
+	}
+
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			matrix[i][j] = rand() % 100;
+		}
+	}
+	return matrix;
+}
+
+void printMatrix(int **matrix, int size) {
+	int i, j;
+	for (i = 0; i < size; i++) {
+		printf("[ ");
+		for (j = 0; j < size; j++) {
+			printf("%d ", matrix[i][j]);
+		}
+		printf("]\n");
+	}
+}
+
+int** multiplyMatrix (int **firstMatrix, int **secondMatrix, int size) {
+	int** finalMatrix = malloc(size * sizeof(int *));
+	int i, j, z, sum;
+
+	for (i = 0; i < size; i++) {
+		finalMatrix[i] = malloc(size * sizeof(int));
+	}
+
+	for (i = 0; i < size; i++) {
+	      for (j = 0; j < size; j++) {
+	        for (z = 0; z < size; z++) {
+	          sum = sum + firstMatrix[i][z]*secondMatrix[z][j];
+	        }
+
+	        finalMatrix[i][j] = sum;
+	        sum = 0;
+	      }
+	 }
+	return finalMatrix;
 }
