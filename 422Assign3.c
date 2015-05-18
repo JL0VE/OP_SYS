b43e437cf4926cd1263e2b63f3d81ca5e651570a
dkenyon <dkenyon@uw.edu>
2015-05-18 16:06:49 -0700

    Update 422Assign3.c
    update three with global scheduler

--- 422Assign3.c
+++ 422Assign3.c
@@ -14,15 +14,6 @@
 #define NUMBER_OF_JOBS 10
 #define CLOCK_CYCLE 2
 
-static int jobCounter = 0;
-static int jobsFinished = 0;
-pthread_mutex_t lockCPU = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t lockIO = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t lockFinal = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t lockCounter = PTHREAD_MUTEX_INITIALIZER;
-//time_t now;
-//struct tm *tm;
-
 typedef struct job {
 	int job_id;
 	int current_phase; // 0 = cpu, 1 = io, 2 = finished
@@ -44,37 +35,65 @@ typedef struct queues {
 	int finishedSize;
 } SchedulerQueues;
 
+SchedulerQueues theQueues;
+static int jobCounter = 0;
+static int jobsFinished = 0;
+pthread_mutex_t lockCPU;
+pthread_mutex_t lockIO;
+pthread_mutex_t lockFinal;
+pthread_mutex_t lockCounter;
+//time_t now;
+//struct tm *tm;
+
+
+
 //function declarations
-void initScheduler(SchedulerQueues *theQueues);
-Job dequeue(SchedulerQueues *theQueues, int whichQueue);
-void enqueue(SchedulerQueues *theQueues, Job theJob, int whichQueue);
+void initScheduler();
+Job dequeue(int whichQueue);
+void enqueue(Job theJob, int whichQueue);
 void initJob(Job *theJob);
-void* cpu(SchedulerQueues *theQueues);
-void* io(SchedulerQueues *theQueues);
-void* job(SchedulerQueues *theQueues);
+void* cpu();
+void* io();
+void* job();
 
 //main
 int main(int argc, char** argv) {
 	setvbuf(stdout, NULL, _IONBF, 0);
 	srand(time(NULL));
 	pthread_t cpuOne[8], ioOne[4], jobOne[4];
-	int i;
+	int i, rc;
 
-	SchedulerQueues theQueues;
-	initScheduler(&theQueues);
+
+	initScheduler();
 
 //	printf("job counter: %d\n", jobCounter);
+	pthread_mutex_init(&lockCPU, NULL);
+	pthread_mutex_init(&lockIO, NULL);
+	pthread_mutex_init(&lockFinal, NULL);
+	pthread_mutex_init(&lockCounter, NULL);
 
 	for (i = 0; i < 4; i++) {
-		pthread_create(&jobOne[i], NULL, job, &theQueues);
+		printf("current job thread: %d\n", i);
+		rc = pthread_create(&(jobOne[i]), NULL, job, NULL);
+		printf("\n\nCPU Size: %d\n", theQueues.cpuSize);
+		printf("\n\nIO Size: %d\n", theQueues.ioSize);
+		printf("\n\nFinished Size: %d\n", theQueues.finishedSize);
+
 	}
 	for (i = 0; i < 8; i++) {
-		pthread_create(&cpuOne[i], NULL, cpu, &theQueues);
+		printf("current cpu thread: %d\n", i);
+		rc = pthread_create(&(cpuOne[i]), NULL, cpu, NULL);
+
 	}
 	for (i = 0; i < 4; i++) {
-		pthread_create(&ioOne[i], NULL, io, &theQueues);
+		printf("current io thread: %d\n", i);
+		rc = pthread_create(&(ioOne[i]), NULL, io, NULL);
 	}
 
+	pthread_join(jobOne[0], NULL);
+	pthread_join(jobOne[1], NULL);
+	pthread_join(jobOne[2], NULL);
+	pthread_join(jobOne[3], NULL);
 	pthread_join(cpuOne[0], NULL);
 	pthread_join(cpuOne[1], NULL);
 	pthread_join(cpuOne[2], NULL);
@@ -87,22 +106,21 @@ int main(int argc, char** argv) {
 	pthread_join(ioOne[1], NULL);
 	pthread_join(ioOne[2], NULL);
 	pthread_join(ioOne[3], NULL);
-	pthread_join(jobOne[0], NULL);
-	pthread_join(jobOne[1], NULL);
-	pthread_join(jobOne[2], NULL);
-	pthread_join(jobOne[3], NULL);
-
+	pthread_mutex_destroy(&lockCPU);
+	pthread_mutex_destroy(&lockIO);
+	pthread_mutex_destroy(&lockFinal);
+	pthread_mutex_destroy(&lockCounter);
 	return 1;
 }
 
-void initScheduler(SchedulerQueues *theQueues) {
-	theQueues->cpuSize = 0;
-	theQueues->ioSize = 0;
-	theQueues->finishedSize = 0;
+void initScheduler() {
+	theQueues.cpuSize = 0;
+	theQueues.ioSize = 0;
+	theQueues.finishedSize = 0;
 }
 
 void initJob(Job *theJob) {
-	pthread_mutex_lock(&lockCounter);
+	pthread_mutex_trylock(&lockCounter);
 	theJob->job_id = jobCounter++;
 	theJob->current_phase = 0;
 	theJob->cpuDuration = (rand() % 5) + 1;
@@ -114,128 +132,146 @@ void initJob(Job *theJob) {
 //	sleep(2);
 }
 
-void enqueue(SchedulerQueues *theQueues, Job theJob, int whichQueue) {
+void enqueue(Job theJob, int whichQueue) {
+	printf("queue: %d\n", whichQueue);
 	switch (whichQueue) {
 	case 0:
-		pthread_mutex_lock(&lockCPU);
-		theQueues->cpuQueue[theQueues->cpuSize] = theJob;
-		theQueues->cpuSize++;
+		pthread_mutex_trylock(&lockCPU);
+		printf("cpu size before increment: %d\n", theQueues.cpuSize);
+		theQueues.cpuQueue[theQueues.cpuSize] = theJob;
+		theQueues.cpuSize++;
+		printf("cpu size after increment: %d\n", theQueues.cpuSize);
 		pthread_mutex_unlock(&lockCPU);
 		break;
 	case 1:
-		pthread_mutex_lock(&lockIO);
-		theQueues->cpuQueue[theQueues->cpuSize] = theJob;
-		theQueues->ioSize++;
+		pthread_mutex_trylock(&lockIO);
+		theQueues.ioQueue[theQueues.ioSize] = theJob;
+		printf("Should increment iosize from %d to %d\n", theQueues.ioSize,
+				theQueues.ioSize + 1);
+		theQueues.ioSize++;
+		printf("iosize is now: %d\n", theQueues.ioSize);
 		pthread_mutex_unlock(&lockIO);
 		break;
 	case 2:
-		pthread_mutex_lock(&lockFinal);
-		theQueues->cpuQueue[theQueues->cpuSize] = theJob;
-		theQueues->finishedSize++;
+		pthread_mutex_trylock(&lockFinal);
+		theQueues.finishedQueue[theQueues.finishedSize] = theJob;
+		theQueues.finishedSize++;
 		pthread_mutex_unlock(&lockFinal);
 		break;
 	}
 }
 
-Job dequeue(SchedulerQueues *theQueues, int whichQueue) {
+Job dequeue(int whichQueue) {
 	Job returnJob;
 	int i;
 	switch (whichQueue) {
 	case 0:
-		pthread_mutex_lock(&lockCPU);
-		returnJob = theQueues->cpuQueue[0];
-		for (i = 0; i < theQueues->cpuSize; i++) {
-			theQueues->cpuQueue[i] = theQueues->cpuQueue[i + 1];
+		pthread_mutex_trylock(&lockCPU);
+		returnJob = theQueues.cpuQueue[0];
+		for (i = 0; i < theQueues.cpuSize; i++) {
+			theQueues.cpuQueue[i] = theQueues.cpuQueue[i + 1];
 		}
-		theQueues->cpuSize--;
+		theQueues.cpuSize--;
 		pthread_mutex_unlock(&lockCPU);
 		break;
 	case 1:
-		pthread_mutex_lock(&lockIO);
-		returnJob = theQueues->ioQueue[0];
-		for (i = 0; i < theQueues->cpuSize; i++) {
-			theQueues->ioQueue[i] = theQueues->ioQueue[i + 1];
+		pthread_mutex_trylock(&lockIO);
+		returnJob = theQueues.ioQueue[0];
+		for (i = 0; i < theQueues.ioSize; i++) {
+			theQueues.ioQueue[i] = theQueues.ioQueue[i + 1];
 		}
-		theQueues->ioSize--;
+		theQueues.ioSize--;
 		pthread_mutex_unlock(&lockIO);
 		break;
 	case 2:
-		pthread_mutex_lock(&lockFinal);
-		returnJob = theQueues->finishedQueue[0];
-		for (i = 0; i < theQueues->cpuSize; i++) {
-			theQueues->finishedQueue[i] = theQueues->finishedQueue[i + 1];
+		pthread_mutex_trylock(&lockFinal);
+		returnJob = theQueues.finishedQueue[0];
+		for (i = 0; i < theQueues.finishedSize; i++) {
+			theQueues.finishedQueue[i] = theQueues.finishedQueue[i + 1];
 		}
-		theQueues->finishedSize--;
+		theQueues.finishedSize--;
 		pthread_mutex_unlock(&lockFinal);
 		break;
 	}
 	return returnJob;
 }
 
-void* cpu(SchedulerQueues *theQueues) {
+void* cpu() {
 	Job job;
 	int jobTime;
 	while (jobsFinished < NUMBER_OF_JOBS) {
-		if (theQueues->cpuSize == 0) {
+		if (theQueues.cpuSize == 0) {
 			sleep(2);
+		} else {
+			printf("about to be dequeue'd from CPU: job %d\n", jobCounter);
+			//	time(&rawTime);
+			//	printf("system time: %s\n", ctime(&rawTime));
+			job = dequeue(0);
+			printf("CPU dequeue'd: job %d\n", jobCounter);
+			//	time(&rawTime);
+			//	printf("system time: %s\n", ctime(&rawTime));
+			jobTime = job.cpuDuration;
+			//	sleep(jobTime);
+			printf("about to be enqueue'd to IO: job %d;\n", jobCounter);
+			printf("iosize: %d\n", theQueues.ioSize);
+			enqueue(job, 1);
+			printf("iosize after enqueue: %d\n", theQueues.ioSize);
+			printf("IO enqueue'd: job %d\n", jobCounter);
 		}
-		printf("about to be dequeue'd from CPU: job %d\n", jobCounter);
-		//	time(&rawTime);
-		//	printf("system time: %s\n", ctime(&rawTime));
-		job = dequeue(&theQueues, 0);
-		printf("CPU dequeue'd: job %d\n", jobCounter);
-		//	time(&rawTime);
-		//	printf("system time: %s\n", ctime(&rawTime));
-		jobTime = job.cpuDuration;
-		//	sleep(jobTime);
-		printf("about to be enqueue'd to IO: job %d\n", jobCounter);
-		enqueue(&theQueues, job, 1);
-		printf("IO enqueue'd: job %d\n", jobCounter);
+
 	}
 	return NULL;
 }
 
-void* io(SchedulerQueues *theQueues) {
+void* io() {
 	Job job;
 	int jobTime;
 	while (jobsFinished < NUMBER_OF_JOBS) {
-		if (theQueues->ioSize == 0) {
+		if (theQueues.ioSize == 0) {
 			sleep(2);
+		} else {
+			printf("about to be dequeue'd from IO: job %d\n", jobCounter);
+			job = dequeue(1);
+			printf("dequeue'd from IO: job %d\n", jobCounter);
+			jobTime = job.ioDuration;
+			//	sleep(jobTime);
+			printf("about to be enqueue'd to final: job %d\n", jobCounter);
+			enqueue(job, 2);
+			printf("final enqueue'd: job %d\n", jobCounter);
 		}
-		printf("about to be dequeue'd from IO: job %d\n", jobCounter);
-		job = dequeue(&theQueues, 1);
-		printf("dequeue'd from IO: job %d\n", jobCounter);
-		jobTime = job.ioDuration;
-		//	sleep(jobTime);
-		printf("about to be enqueue'd to final: job %d\n", jobCounter);
-		enqueue(&theQueues, job, 2);
-		printf("final enqueue'd: job %d\n", jobCounter);
+
 	}
 
 	return NULL;
 }
 
-void* job(SchedulerQueues *theQueues) {
+void* job() {
 	Job job;
 	while (jobsFinished < NUMBER_OF_JOBS) {
-		if (theQueues->finishedSize == 0) {
-			sleep(2);
-		}
+//		if (theQueues.finishedSize == 0) {
+//			sleep(2);
+//		} else
 		if (jobCounter < NUMBER_OF_JOBS) {
 			initJob(&job);
-			printf("about to be enqueue'd to CPU: job %d\n", jobCounter);
-			enqueue(&theQueues, job, 0);
+			printf("about to be enqueue'd to CPU: job %d\n"
+					"current thread: %d\n", jobCounter, pthread_self);
+			enqueue(job, 0);
+//			printf("cpu size after enqueue: %d\n", theQueues.cpuSize);
 			printf("CPU enqueue'd: job %d\n", jobCounter);
 			//		now = time(0);
 			//		tm = localtime(&now);
-			//		printf("system time in seconds: %d\n", tm->tm_sec);
+			//		printf("system time in seconds: %d\n", tm.tm_sec);
 			//		sleep(2);
 			//		now = time(0);
 			//		tm = localtime(&now);
-			//		printf("system time in seconds: %d\n", tm->tm_sec);
-		} else if (theQueues->finishedSize > 0) {
+			//		printf("system time in seconds: %d\n", tm.tm_sec);
+
+			printf("\n\nCPU Size: %d\n", theQueues.cpuSize);
+			printf("\n\nIO Size: %d\n", theQueues.ioSize);
+			printf("\n\nFinished Size: %d\n", theQueues.finishedSize);
+		} else if (theQueues.finishedSize > 0) {
 			printf("about to be dequeue'd from final: job %d\n", jobCounter);
-			job = dequeue(&theQueues, 2); //DQ from finished queue
+			job = dequeue(2); //DQ from finished queue
 			printf("dequeue'd from final: job %d\n", jobCounter);
 			free(&job);
 			jobsFinished++;
@@ -243,10 +279,6 @@ void* job(SchedulerQueues *theQueues) {
 	}
 	pthread_exit(NULL);
 	return NULL;
-}
-
-int jobsStillLingering() {
-
 }
 
 //	Job job1;
